d428f456c14488bd2bd5d1e2b2998da416c89431
neel <neel2292@gmail.com>
2019-06-16 18:45:27 +0800

    Added Prompt the message yearly “Reminder : Kindly update your personal profile if you have not done so”

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 import {PruliaMemberProvider} from "../../providers/prulia-member/prulia-member";
 import {PruliaHomeProvider} from "../../providers/prulia-home/prulia-home";
 import {Events} from 'ionic-angular/util/events';
@@ -10,7 +10,7 @@ import {HomeDetailPage} from "../home-detail/home-detail"
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController,
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,
               public memberProvider: PruliaMemberProvider, public homeProvider: PruliaHomeProvider, public appEvent: Events) {
 
   }
@@ -20,11 +20,29 @@ export class HomePage {
     this.appEvent.publish('loading:start', 'Loading...');
 
     this.memberProvider.get_member_profile(true)
-      .then(data => {
+      .then((data: any) => {
+          let cur_date = new Date().getTime(),
+            modified_date,
+            alert;
+
+          //set a yearly reminder for updating profile
+          if (data) {
+            modified_date = new Date(data.modified).getTime();
+            if (((cur_date - modified_date) / (1000 * 60 * 60 * 24)) > 365) {
+              alert = this.alertCtrl.create({
+                title: 'Yearly reminder',
+                message: "Reminder : Kindly update your personal profile if you have not done so",
+                buttons: ['OK']
+              });
+
+              alert.present();
+            }
+          }
+
         }, (error => {
           console.log(error)
         })
-      )
+      );
     this.homeProvider.get_home_listing(true)
       .then(data => {
           this.appEvent.publish('loading:end');
